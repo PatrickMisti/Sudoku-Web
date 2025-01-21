fd14f9fb92dc2e2afeab5fbae8afa34fec8b91ef
PatrickMisti <patrickm99@hotmail.de>
2025-01-21 23:08:24 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ImprintComponent} from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, ImprintComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Sudoku';
