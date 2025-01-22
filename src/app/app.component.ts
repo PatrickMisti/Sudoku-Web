f3600aa16f7d4c0d8b57968b3069d475254bfc93
PatrickMisti <patrickm99@hotmail.de>
2025-01-22 19:23:45 +0100

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ImprintComponent} from './imprint/imprint.component';
+import {ImprintComponent} from './components/imprint/imprint.component';
+import {StartComponent} from './components/start/start.component';
+import {SudokuGridComponent} from './components/sudoku-grid/sudoku-grid.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FlexLayoutServerModule} from '@angular/flex-layout/server';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ImprintComponent],
+  imports: [StartComponent, FlexLayoutModule, FlexLayoutServerModule, SudokuGridComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
